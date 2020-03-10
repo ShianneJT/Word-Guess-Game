5993fbc7aec8094293735bd4e7a1e9c231797e1f
Shianne <ShianneJT@gmail.com>
2020-03-09 19:48:56 -0600

    Basic random word generator added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,24 @@
 // then checks it against the chosen word
 //
 
+// Word List AKA Characters from The Office
+    var officeChars = ["Michael Scott", "Pam Beesly", "Jim Halpert", "Dwight Schrute", 
+        "Stanley Hudson", "Kevin Malone", "Angela Martin", "Phylis Vance", "Bob Vance of Vance Refrigeration", 
+        "Meredith Palmer", "Creed Bratton", "Oscar Martinez", "Ryan Howard", "Kelly Kapoor", "Andy Bernard",
+        "Toby Flenderson", "Darryl Philbin", "Moes", "Jan Levinson", "David Wallace", "Robert California",
+        "Holly Flax", "Todd Packer"];
+
+    var charChoice = officeChars[Math.floor(Math.random()*officeChars.length)];
+
+
+
+
+
+
+
+
+
+
 
 // This checks to make sure the user put in a letter and not a silly character
 
@@ -18,6 +36,3 @@
     } else {
         console.log("Choose a letter!");
     }
-
-
-
