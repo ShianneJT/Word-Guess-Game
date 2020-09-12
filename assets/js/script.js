1087388e8402a6a18465cab9904ed4045b1f926f
Shianne <ShianneJT@gmail.com>
2020-09-12 09:23:27 -0600

    displays character to dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,14 @@ let wins;
 let losses;
 let lives = 10;
 
+// characters
+const officeCharacters = ["michael scott", "pam beesly", "jim halpert", "dwight schrute",
+  "stanley hudson", "kevin malone", "angela martin", "phylis vance",
+  "meredith palmer", "creed bratton", "oscar martinez", "ryan howard", "kelly kapoor", "andy bernard",
+  "toby flenderson", "darryl philbin", "moes", "jan levinson", "david wallace", "robert california",
+  "holly flax", "todd packer", "bob vance of vance refrigeration"];
+console.log(officeCharacters);
+
 const CreateButtons = () => {
   for (let i = 0; i < 26; i++) {
     let char = String.fromCharCode(65 + i);
@@ -17,3 +25,30 @@ const CreateButtons = () => {
 }
 
 CreateButtons();
+
+// grab random character
+let randomCharacter = officeCharacters[Math.floor(Math.random() * officeCharacters.length)];
+randomCharacter = randomCharacter.toUpperCase();
+console.log("Random character: " + randomCharacter);
+
+// display the random character to the dom with letters hidden
+const hiddenWordDisplay = randomCharacter.split("");
+console.log('hiddenWord: ' + hiddenWordDisplay)
+
+for (let i = 0; i < hiddenWordDisplay.length; i++) {
+  if (hiddenWordDisplay[i] === " ") {
+    $("#wordContainer").append("<span id='space'> - </span>");
+
+  } else {
+    $("#wordContainer").append("<span id='letter_" + i + "'> _ </span>");
+  }
+}
+
+console.log('hidden word display: ' + hiddenWordDisplay);
+
+
+
+
+
+
+
