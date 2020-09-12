595b5260c13de1c57a91c9ae2fffe8d579b88ec2
Shianne <ShianneJT@gmail.com>
2020-09-12 08:38:36 -0600

    Alphabet buttons

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+let wins;
+let losses;
+let lives = 10;
+
+const CreateButtons = () => {
+  for (let i = 0; i < 26; i++) {
+    let char = String.fromCharCode(65 + i);
+
+    let letterButton = $("<button onclick='this.disabled = true;'>");
+    letterButton.text(char);
+    letterButton.val(char);
+    letterButton.addClass("btn btn-primary letterBtn");
+
+    $("#alphabetContainer").append($(letterButton));
+    console.log(letterButton.val())
+  }
+}
+
+CreateButtons();
