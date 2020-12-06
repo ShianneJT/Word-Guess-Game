71db0fbc88a8383fdfbed3b69e065871d94c23be
unknown <shiannejt@gmail.com>
2020-12-06 14:06:31 -0700

    functioning needs styling

--- v2/assets/js/script.js
+++ v2/assets/js/script.js
@@ -3,61 +3,67 @@ let wins = 0;
 let losses = 0;
 let lives = 5;
 let lettersArray = [];
+let gameWon = false;
 
 $("#newGameBtn").hide();
 $("#gameContainer").hide();
 $("#wins").html("<span>Wins: " + wins + "</span>");
 $("#losses").html("<span>Losses: " + losses + "</span>");
-$("#livesLeft").html("<span>Lives: " + lives + "</span>");
+$("#livesLeft").html("<span>Lives Remaining: " + lives + "</span>");
 
-// characters
-const officeCharacters = ["michael scott", "pam beesly", "jim halpert", "dwight schrute",
-  "stanley hudson", "kevin malone", "angela martin", "phylis vance",
-  "meredith palmer", "creed bratton", "oscar martinez", "ryan howard", "kelly kapoor", "andy bernard",
-  "toby flenderson", "darryl philbin", "moes", "jan levinson", "david wallace", "robert california",
-  "holly flax", "todd packer", "bob vance of vance refrigeration"];
 
-// grab random character
-let randomCharacter = officeCharacters[Math.floor(Math.random() * officeCharacters.length)];
-randomCharacter = randomCharacter.toUpperCase();
-console.log("Random character: " + randomCharacter);
+const startGame = () => {
+  $("#startBtn").hide();
+  $("#gameContainer div").empty();
 
-// create alphabet buttons
-const createButtons = () => {
-  for (let i = 0; i < 26; i++) {
-    let char = String.fromCharCode(65 + i);
+  let i = (Math.floor(Math.random() * 21) + 1);
+  imgSrc = "assets/images/office" + i + ".jpg"
 
-    let letterButton = $("<button onclick='this.disabled = true;'>");
-    letterButton.text(char);
-    letterButton.val(char);
-    letterButton.addClass("btn btn-primary letterBtn");
+  const officeCharacters = ["Michael Scott", "Pam Beesly", "Jim Halpert", "Dwight Schrute",
+    "Stanley Hudson", "Kevin Malone", "Angela Martin", "Phylis Vance",
+    "Meredith Palmer", "Creed Bratton", "Oscar Martinez", "Ryan Howard", "Kelly Kapoor", "Andy Bernard",
+    "Toby Flenderson", "Darryl Philbin", "Moes", "Jan Levinson", "David Wallace", "Robert California",
+    "Holly Flax", "Todd Packer", "Bob Vance of Vance Refrigeration"];
 
-    $("#alphabetContainer").append($(letterButton));
-  };
-};
+  // grab random character
+  let randomCharacterName = officeCharacters[Math.floor(Math.random() * officeCharacters.length)];
+  let randomCharacter = randomCharacterName.toUpperCase();
+  console.log("Random character: " + randomCharacter);
 
-const displayCharacter = () => {
-  // display the random character to the dom with letters hidden
-  const hiddenWordDisplay = randomCharacter.split("");
+  // create alphabet buttons
+  const createButtons = () => {
+    for (let i = 0; i < 26; i++) {
+      let char = String.fromCharCode(65 + i);
 
-  for (let i = 0; i < hiddenWordDisplay.length; i++) {
-    if (hiddenWordDisplay[i] === " ") {
-      $("#wordContainer").append("<span id='space'> - </span>");
-    } else {
-      $("#wordContainer").append("<span id='letter_" + i + "'> _ </span>");
+      let letterButton = $("<button onclick='this.disabled = true;'>");
+      letterButton.text(char);
+      letterButton.val(char);
+      letterButton.addClass("btn btn-primary letterBtn");
+
+      $("#alphabetContainer").append($(letterButton));
     };
   };
-  console.log('hidden word display: ' + hiddenWordDisplay);
-};
 
-// create an array of letters from character
-const letters = randomCharacter.split("");
-const lettersFiltered = letters.filter(letter => letter.trim().length > 0);
-let lettersRemaining = lettersFiltered.length;
+  const displayCharacter = () => {
+    // display the random character to the dom with letters hidden
+    const hiddenWordDisplay = randomCharacter.split("");
 
-// handle button click
-$("#startBtn").click(function () {
-  $("#startBtn").hide();
+    for (let i = 0; i < hiddenWordDisplay.length; i++) {
+      if (hiddenWordDisplay[i] === " ") {
+        $("#wordContainer").append("<span id='space'> - </span>");
+      } else {
+        $("#wordContainer").append("<span id='letter_" + i + "'> _ </span>");
+      };
+    };
+    console.log('hidden word display: ' + hiddenWordDisplay);
+  };
+
+  // create an array of letters from character
+  const letters = randomCharacter.split("");
+  const lettersFiltered = letters.filter(letter => letter.trim().length > 0);
+  let lettersRemaining = lettersFiltered.length;
+
+
   $("#gameContainer").show();
   createButtons();
   displayCharacter();
@@ -84,33 +90,55 @@ $("#startBtn").click(function () {
       };
     } else {
       lives--;
-      $("#livesLeft").html("<span>Lives: " + lives + "</span>");
+      $("#livesLeft").html("<span>Lives Remaining: " + lives + "</span>");
       if (lives === 0) {
       };
     };
     checkWin();
   });
+
+  checkWin = () => {
+    if (lives == 0) {
+      losses++;
+      gameWon = false;
+      $("#losses").html("<span>Losses: " + losses + "</span>");
+      launchModal();
+      console.log("you lost...");
+    } else if (lives > 0 && lettersRemaining == 0) {
+      wins++;
+      gameWon = true;
+      $("#wins").html("<span>Wins: " + wins + "</span>");
+      launchModal();
+      console.log("you won...");
+    };
+  };
+
+  const launchModal = () => {
+    $("#modalBody").html('<img src="' + imgSrc + '">');
+
+
+    if (gameWon) {
+      $(".modal-title").text("You got it!");
+      $(".newGame").text("Play Again?");
+      $("#gameOverModal").modal("show");
+    } else {
+      $(".modal-title").text("Sorry! The character was " + randomCharacterName);
+      $(".newGame").text("Try Again?");
+      $("#gameOverModal").modal("show");
+    }
+  }
+}
+
+// handle button click
+$(".startBtn, .playAgain").click(function () {
+  lives = 5;
+  $("#livesLeft").html("<span>Lives Remaining: " + lives + "</span>");
+  startGame();
 });
 
-checkWin = () => {
-  if (lives == 0) {
-    $("#winnerModal").modal("show");
-    $("#won").hide();
-    console.log("you lost...")
-    losses++;
-    $("#losses").html("<span>Losses: " + losses + "</span>");
-    $("#startBtn").show();
-    //$("#alphabetContainer").hide();
-  } else if (lives > 0 && lettersRemaining == 0) {
-    $("#winnerModal").modal("show");
-    $("#lost").hide();
-
-    console.log("you won...")
-    // alert("you won...")
-    wins++;
-    $("#wins").html("<span>Wins: " + wins + "</span>");
-    $("#alphabetContainer").hide();
-  }
-  // new game
-  // reset stats
-}
+// characters
+// const officeCharacters = ["michael scott", "pam beesly", "jim halpert", "dwight schrute",
+//   "stanley hudson", "kevin malone", "angela martin", "phylis vance",
+//   "meredith palmer", "creed bratton", "oscar martinez", "ryan howard", "kelly kapoor", "andy bernard",
+//   "toby flenderson", "darryl philbin", "moes", "jan levinson", "david wallace", "robert california",
+//   "holly flax", "todd packer", "bob vance of vance refrigeration"];
